85d1d9666f1c61eadedccf77c021c1e205d74fe2
Mohamed Hossny <mhossny2@outlook.com>
2024-05-09 07:58:46 +0300

    chore: switch to postgres database

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.CharField(max_length=100)
+  image = models.URLField()
+  rating = models.FloatField(null=True)
+  views = models.IntegerField(null=True)
+  description = models.TextField()
--- books/views.py
+++ books/views.py
@@ -1,77 +1,49 @@
 from django.shortcuts import render,redirect
-# Create your views here.
-
-books = [
-    {
-        "id": 1,
-        "title": "Book Title",
-        "author": "Book Author",
-        "image": "https://placehold.co/300x150",
-        "rating": 4.5,
-        "views": 100,
-        "description": "A brief description of the book.",
-    },
-    {
-        "id": 2,
-        "title": "Book Title",
-        "author": "Book Author",
-        "image": "https://placehold.co/300x150",
-        "rating": 4.5,
-        "views": 100,
-        "description": "A brief description of the book.",
-    },
-    {
-        "id": 3,
-        "title": "Book Title",
-        "author": "Book Author",
-        "image": "https://placehold.co/300x150",
-        "rating": 4.5,
-        "views": 100,
-        "description": "A brief description of the book.",
-    },
-]
-
+from .models import Book
 
 def index(request):
-    context = {"books": books, "title": "Home"}
-    print("len", len(books))
+    context = {"books": Book.objects.all(), "title": "Home"}
+    print("len", context.get("books").count())
     return render(request, "books/index.html", context)
 
 def add(request):
     if request.method == "POST":
-        books.append({
-            "id": len(books) + 1,
-            "title": request.POST.get("title"),
-            "author": request.POST.get("author"),
-            "image": request.POST.get("image"),
-            "rating": request.POST.get("rating"),
-            "views": request.POST.get("views"),
-            "description": request.POST.get("description"),
-        })
-        print("books", books)
+        book = Book.objects.create(
+            title=request.POST.get("title"),
+            author=request.POST.get("author"),
+            image=request.POST.get("image"),
+            rating=request.POST.get("rating"),
+            views=request.POST.get("views"),
+            description=request.POST.get("description"),
+        )
+        book.save()
+        print("book", book)
         return redirect("/")
     else:
         return render(request, "books/add.html", {"title": "Add New Book"})
 
 def detail(request, id):
-    context = {"book": books[id - 1], "title": "Book - " + str(id)}
+    book = Book.objects.get(id=id)
+    context = {"book": book}
     print(context["book"])
     return render(request, "books/detail.html", context)
 
 def edit(request, id):
+    book=Book.objects.get(id=id)
     if request.method == "POST":
-        books[id - 1]["title"] = request.POST.get("title")
-        books[id - 1]["author"] = request.POST.get("author")
-        books[id - 1]["image"] = request.POST.get("image")
-        books[id - 1]["rating"] = request.POST.get("rating")
-        books[id - 1]["views"] = request.POST.get("views")
-        books[id - 1]["description"] = request.POST.get("description")
-        print("books", books)
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.image = request.POST.get("image")
+        book.rating = request.POST.get("rating")
+        book.views = request.POST.get("views")
+        book.description = request.POST.get("description")
+        book.save()
+        print("book", book.rating)
         return redirect("/")
     else:
-        context = {"book": books[id - 1], "title": "Edit Book - " + str(id)}
+        context = {"book": book, "title": "Edit Book - " + str(id)}
         return render(request, "books/edit.html", context)
 
 def delete(request, id):
-    books.pop(id - 1)
+    Book.objects.get(id=id).delete()
     return redirect("/")
